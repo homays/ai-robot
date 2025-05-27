fef58104dad7943e93eb2581154ab450e04f823f
huangrui <2362552845@qq.com>
2025-05-27 16:28:02 +0800

    feat: Spring AI 第一次提交

---
+++ ymkx-ai-robot-springboot/src/main/java/com/ymkx/ai/robot/YmkxAiRobotSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ymkx.ai.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmkxAiRobotSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmkxAiRobotSpringbootApplication.class, args);
+    }
+
+}
